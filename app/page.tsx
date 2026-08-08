0922d9e69fc417c291a47dbb853f8c7878e90f1f
NitshianecGo <tanz.devil.nitshianec@gmail.com>
2026-08-08 13:25:47 +0900

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation"
-
-// The app itself is a fully self-contained single-file SPA at /public/index.html.
-// This route simply forwards the v0 preview (and any deployment root) to it.
-export default function Page() {
-  redirect("/index.html")
-}
